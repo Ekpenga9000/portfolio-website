41b308fdb51b76b99bd5bd99fd38de4bdb99ee2a
Louis Ekpenga <louisekpenga@MBPl20FVFHHBUYQ05D.ht.home>
2023-11-24 01:03:47 -0500

    Create scarfolding for the rest of the application.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>This site currently under construction... And we'll be up soon </p>
+    </>
+  )
+}
+
+export default App
