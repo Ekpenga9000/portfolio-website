1455644c8c6aea6bd217dda2a16719daa60807c6
Louis Ekpenga <louisekpenga@MBPl20FVFHHBUYQ05D.ht.home>
2023-11-26 20:08:21 -0500

    Implement break-point for different viewports

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,16 +1,33 @@
 import "./NavBar.scss";
-
+import {useState} from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoCloseOutline } from "react-icons/io5";
 const NavBar = () =>{
+    const [isDropdown, setIsDropdown] = useState(false); 
+
+    const handleMenu = () =>{
+        setIsDropdown(!isDropdown);
+    }
     return(
         <nav className="nav">
             <h3 className="nav__logo">Omogbare Louis Ekpenga</h3>
 
-            <ul className="nav__menu">
+            <div className="nav__btn" onClick={handleMenu}>{!isDropdown && <RxHamburgerMenu />}{isDropdown && <IoCloseOutline />}</div>
+
+            <ul className="nav__menu--desktop">
                 <li className="nav__item">About me</li>
                 <li className="nav__item">Projects</li>
                 <li className="nav__item">Contact me</li>
                 <li className="nav__item">Resume</li>
             </ul>
+
+            {isDropdown && <ul className="nav__dropdown">
+                <li className="nav__item">About me</li>
+                <li className="nav__item">Projects</li>
+                <li className="nav__item">Contact me</li>
+                <li className="nav__item">Resume</li>
+            </ul>}
+
         </nav>
     )
 }
