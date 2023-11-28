a16de121e6a72621e88b867647a32a9e03892ef9
Louis Ekpenga <louisekpenga@MBPl20FVFHHBUYQ05D.local>
2023-11-28 16:13:58 -0500

    Add content to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,24 @@
 import "./Footer.scss";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
 
-const Footer = () =>{
-    return(
+
+const Footer = () => {
+    return (
         <footer className="footer">
             <div className="footer__container">
-               <h3 className="footer__title">Contact me</h3>
+                <div className="footer__icons-div">
+                    <a href="https://github.com/Ekpenga9000"  className="footer__icon">
+                    <FaGithub />
+                    </a>
+                    <a href="https://www.linkedin.com/in/omogbareekpenga/" className="footer__icon">
+                    <FaLinkedin />
+                    </a>
+                    <a href="mailto:ekpenga9000@gmail.com" className="footer__icon">
+                    <BiLogoGmail />
+                    </a>
+                </div>
+                <p className="footer__copyright">&copy; 2023 Omogbare Louis Ekpenga. All rights reserved.</p>
             </div>
         </footer>
     )
