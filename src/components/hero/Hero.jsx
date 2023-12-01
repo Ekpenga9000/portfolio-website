23c4390b283cd776a821c5e04ce56fd69bbbb29d
Louis Ekpenga <louisekpenga@MBPl20FVFHHBUYQ05D.local>
2023-12-01 14:34:03 -0500

    Pushing the last of my stuff.

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,20 +1,26 @@
 import "./Hero.scss";
-import { useEffect, useRef } from "react";
+import { useRef, useLayoutEffect } from "react";
 import {gsap} from "gsap"; 
 
 const Hero = () => {
-    let titleItem = useRef(null); 
-    let textItem = useRef(null); 
-    let rocketItem = useRef(null);
+    const titleItem = useRef(); 
+    const textItem = useRef(); 
+    const rocketItem = useRef();
+    const comp = useRef();
 
-    useEffect(()=>{
-        gsap.to(titleItem.current, { y:-20, opacity:1, duration:3, ease:"power2.inOut"});
-        gsap.to(textItem.current, { y:-20, opacity:1, duration:4, ease:"power2.inOut"});
-        gsap.to(rocketItem.current, { y:-20, opacity:1, duration:5, ease:"power2.inOut"});
+    useLayoutEffect(()=>{
+        let ctx = gsap.context(()=>{
+            
+            gsap.to(titleItem.current, { y:-20, opacity:1, duration:3, ease:"power2.inOut"});
+            gsap.to(textItem.current, { y:-20, opacity:1, duration:4, ease:"power2.inOut"});
+            gsap.to(rocketItem.current, { y:-20, opacity:1, duration:5, ease:"power2.inOut"});
+        }, comp); 
+
+        return () => ctx.revert();
     }, [])
 
     return (
-        <section className="hero">
+        <section ref={comp} className="hero">
             <h2 ref={titleItem}className="hero__title">Hey there 👋🏽, fellow human, code voyager!</h2>
             <p ref={textItem} className="hero__text">This <span className="hero__name">Omogbare</span>, your friendly neighborhood full-stack wizard and code enthusiast 👨🏾‍💻✨ reaching from my TechCoven in 📍Alberta.</p>
             <span ref={rocketItem} className="hero__icon">🚀</span>
