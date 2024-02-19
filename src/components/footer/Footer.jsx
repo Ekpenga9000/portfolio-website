999ce2b9c39470f9f59a52b50d4598d3ec723d39
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-02-18 17:03:07 -0700

    Stylefooter component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,30 +6,38 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container container">
-        <a href="#home" className="footer__logo">oe.</a>
+        <a href="#home" className="footer__logo">
+          oe.
+        </a>
         <div className="footer__div">
-          <a href="#about" className="footer__link">About</a>
-          <a href="#skills" className="footer__link">Skills</a>
-          <a href="#projects" className="footer__link">Projects</a>
+          <a href="#about" className="footer__link">
+            About
+          </a>
+          <a href="#skills" className="footer__link">
+            Skills
+          </a>
+          <a href="#projects" className="footer__link">
+            Projects
+          </a>
         </div>
         <div className="footer__div">
           <a href="https://github.com/Ekpenga9000" className="footer__icon">
-            <i className="uil uil-github-alt"></i>
+            <i className="bx bxl-github"></i>
           </a>
 
           <a
             href="https://www.linkedin.com/in/omogbareekpenga/"
             className="footer__icon"
           >
-            <i className="uil uil-linkedin-alt"></i>
+            <i className="bx bxl-linkedin-square"></i>
           </a>
 
           <a href="mailto:ekpenga9000@gmail.com" className="footer__icon">
-          <i className="uil uil-envelope"></i>
+            <i className="bx bxl-gmail"></i>
           </a>
         </div>
         <p className="footer__copyright">
-          Omogbare Louis Ekpenga &copy; 2023. All rights reserved.
+        &copy; Omogbare Louis Ekpenga. All rights reserved.
         </p>
       </div>
     </footer>
