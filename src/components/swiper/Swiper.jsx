956326de5355d3a0e8e61e5583ff8cc1e5292397
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-02-19 00:53:36 -0700

    Add clickable dots to slider

--- src/components/swiper/Swiper.jsx
+++ src/components/swiper/Swiper.jsx
@@ -1,4 +1,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import "./Swiper.scss";
 import img1 from "../../assets/images/kubi-screenshot.png";
 import img2 from "../../assets/images/kubi-screenshot1.png";
@@ -12,11 +15,12 @@ import 'swiper/css';
 export default () => {
     return (
         <Swiper
-            loop={ true }
             spaceBetween={50}
             slidesPerView={1}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
+            grabCursor={true}
+            pagination={{clickable:true}}
+            modules={[Pagination]} 
+            className='mySwiper'
         >
             <SwiperSlide><img src={ img1 } alt="" className='swiper__img'/></SwiperSlide>
             <SwiperSlide><img src={ img2 } alt="" className='swiper__img'/></SwiperSlide>
