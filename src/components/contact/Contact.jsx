fd2cf49bc78982c00033dff80f5c6d22de667396
Louis_Ekpenga <ekpenga9000@gmail.com>
2024-02-19 03:00:11 -0700

    Add email.js service

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import emailjs from "@emailjs/browser";
 import "./Contact.scss";
 
 const Contact = () => {
@@ -10,24 +11,45 @@ const Contact = () => {
   const [message, setMessage] = useState("");
 
   const handleFormChange = (e) => {
-      setFormValue({ ...formValue, [e.target.name]: e.target.value });
+    setFormValue({ ...formValue, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const { name, email, message } = formValue;
     if (!name.trim() || !email.trim() || !message.trim()) {
       setMessage("Please fill all fields.");
       return;
-      }
+    }
+    try {
+      const emailReq = {
+        serviceId: "service_vq5mtvh",
+        templateId: "template_dorbspq",
+        publicKey: "--pjnmFyvLMKxalHz",
+      };
+      const templateParams = {
+        from_name: name,
+        from_email: email,
+        to_name: "Omogbare",
+        message,
+      };
       const initialState = {
-          name: "", 
-          email: "", 
-          message:""
-      }
-      console.log("Form", formValue);
+        name: "",
+        email: "",
+        message: "",
+      };
+      await emailjs.send(
+        emailReq.serviceId,
+        emailReq.templateId,
+        templateParams,
+        emailReq.publicKey
+      );
       setMessage("Message sent!");
       setFormValue({ ...initialState });
+    } catch (err) {
+        console.log(err)
+      setMessage("Error sending message.");
+    }
   };
   return (
     <section className="contact" id="contact">
@@ -39,23 +61,23 @@ const Contact = () => {
             type="text"
             name="name"
             placeholder="Your name"
-                      className="contact__input"
-                      value={formValue.name}
+            className="contact__input"
+            value={formValue.name}
             onChange={handleFormChange}
           />
           <input
             type="email"
             name="email"
             placeholder="Your email"
-                      className="contact__input"
-                      value={formValue.email}
+            className="contact__input"
+            value={formValue.email}
             onChange={handleFormChange}
           />
           <textarea
             name="message"
             placeholder="Your message"
-                      className="contact__txt-area"
-                      value={formValue.message}
+            className="contact__txt-area"
+            value={formValue.message}
             onChange={handleFormChange}
           ></textarea>
           <div className="contact__btn-div">
